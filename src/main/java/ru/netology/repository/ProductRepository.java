51bcfc4994f107b75ea303f88c12e03a93122b2e
Екатерина <88000211+KatShushun@users.noreply.github.com>
2021-11-15 20:44:28 +0300

    Update ProductRepository.java

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package ru.netology.repository;
 import ru.netology.domain.Product;
 
 public class ProductRepository {
+
     private Product[] items = new Product[0];
 
     public void save(Product item) {
@@ -16,15 +17,6 @@ public class ProductRepository {
 
     public Product[] findAll() {
         return items;
-    }
-
-    public Product findById(int id) {
-        for (Product item : items) {
-            if (item.getId() == id) {
-                return item;
-            }
-        }
-        return null;
     }
 
     public void removeById(int id) {
